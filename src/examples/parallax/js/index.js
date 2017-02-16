bdd87037e5b93478f4465294fb39cdfca29b6c80
pilssalgi <heowongeun@gmail.com>
2017-02-16 17:42:22 +0900

    return

---
+++ src/examples/parallax/js/index.js
@@ -0,0 +1,84 @@
+(function () {
+  var SelfPosition = require('../../../js/modules/parallax/SelfPosition');
+  var throttle    = require('throttle-debounce/throttle');
+  function getRandomImage(tags,callBack){
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+    { tags: tags,tagmode: "any",format: "json" },
+    function(data) {
+        var rnd = Math.floor(Math.random() * data.items.length);
+        var imgs = [];
+        for(var i=0; i<data.items.length; i++){
+          var url = data.items[i].media.m.replace('_m','_b');
+          imgs.push({url:url,title:data.items[i].title});
+        }
+
+        callBack(imgs);
+    });
+  }
+
+  var wrap = $('.wrap'),
+      imgs = [];
+  var top = 0, scrollTop = 0;
+  getRandomImage('mountain',function(datas){
+    for(var i=0; i<datas.length; i++){
+
+      var imgWrap = $('<article class="img-wrap"></article>').appendTo(wrap);
+      imgWrap.css({maxWidth:Math.floor(Math.random()*400+300)});
+      var img = document.createElement("img");
+      img.src = datas[i].url;
+      img.onload = function(){
+        $(this).attr({width:this.width,height:this.height});
+      }
+
+      imgWrap.append(img);
+      var title = $('<h3>'+datas[i].title+'</h3>').appendTo(imgWrap);
+      imgs.push({wrap:imgWrap,img:img,y:0,titleY:0,title:title[0],self:new SelfPosition(img)._setup()});
+
+    }
+    setup();
+  });
+
+  function setup(){
+    $(window).on('scroll',onScroll);
+    update();
+  }
+
+  var ticking = false;
+  function onScroll(){
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    console.log('scroll1');
+    if(!ticking){
+      requestAnimationFrame(update);
+      console.log('scroll2');
+    }
+    ticking = true;
+  }
+
+  function update(){
+    top += (scrollTop-top) * 0.1;
+    
+    for(var i=0; i<imgs.length; i++){
+      var self = imgs[i].self;
+      self._update();
+      var y = -5+self.progress*10;
+      imgs[i].y += (y-imgs[i].y)*0.1;
+      var titleY = 100-self.progress*200;
+      imgs[i].titleY += (titleY-imgs[i].titleY)*0.1;
+      if(self.progress < 2 && self.progress > -1){
+        imgs[i].img.style.transform = translate3d('0px',Number(imgs[i].y.toFixed(3))+'%',200);
+        imgs[i].title.style.transform = translate3d('0px',Number(imgs[i].titleY.toFixed(3))+'px',0);
+      }
+    }
+
+    if(Math.abs(top-scrollTop)<0.01){
+      ticking = false;
+    }else{
+      requestAnimationFrame(update);
+    }
+  }
+
+  function translate3d(x,y,z){
+    return 'translate3d('+x+','+y+','+z+'px)';
+  }
+
+}).call(this);
--- src/js/modules/parallax/SelfPosition.js
+++ src/js/modules/parallax/SelfPosition.js
@@ -2,22 +2,30 @@ var windowSize  = require('../util/WindowSize');
 var Bind        = require('../util/Bind');
 var SelfPosition = function(target){
   this.target       = target;
+  this.isFar        = false;
   this.progress     = 0;
   this.progressOld  = 0;
   this.isStageIn    = true;
   this.clientRect   = this.target.getBoundingClientRect();
   this.stageSize    = { width:0, height:0 };
   this.offset       = {left:0,top:0,width:0,height:0};
+  this.option = {
+    margin        : 0,
+    marginTop     : 0,
+    marginBottom  : 0,
+    scale         : 1,
+    mouseMoveInfo : null
+  }
 }
 
 
 SelfPosition.prototype._setup = function(){
-  this._resize = Bind(this._resize,this);
-  $(window).on('_resize',this._resize);
-  this._resize();  
+  this.resize = Bind(this.resize,this);
+  $(window).on('resize',this.resize);
+  this.resize();  
   return this;
 }
-SelfPosition.prototype._resize = function(){
+SelfPosition.prototype.resize = function(){
   this.stageSize      = windowSize();
   this.clientRect     = this.target.getBoundingClientRect();
   this._update();
@@ -25,6 +33,12 @@ SelfPosition.prototype._resize = function(){
 
 SelfPosition.prototype._update = function(scrollY){
   this.clientRect = this.target.getBoundingClientRect();
+  if(this.clientRect.top < this.stageSize.height*1.5){
+    this.isFar = false;
+  }else{
+    this.isFar = true;
+  }
+
   this.progress = 1-((this.clientRect.top+this.clientRect.height)/(this.stageSize.height+this.clientRect.height));
   if(this.progress >= 0 && this.progress <= 1){
     this.isStageIn = true;
