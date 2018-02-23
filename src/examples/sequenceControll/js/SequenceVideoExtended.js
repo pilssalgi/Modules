cf0fe9590cfd83701cc787136e79204a7a1f7bda
wongeun heo <heowongeun@gmail.com>
2018-02-23 20:51:37 +0900

    updated

--- src/examples/sequenceControll/js/SequenceVideoExtended.js
+++ src/examples/sequenceControll/js/SequenceVideoExtended.js
@@ -3,22 +3,22 @@ var SequenceVideo = require('./SequenceVideo');
 function Main($dom,videoID){
 	var sv;
 	var cvs,ctx,video
-	var cvs2,ctx2;
+	var bufferCvs,buffer;
 	var maskImg;
 	function setup(){
 		sv = new SequenceVideo($dom,videoID);
 		cvs 	= sv.getCanvas();
 		ctx 	= sv.getContext();
 
-		cvs2 = document.createElement('canvas');
-		ctx2 = cvs2.getContext('2d');
+		bufferCvs = document.createElement('canvas');
+		buffer 		= bufferCvs.getContext('2d');
 
 		maskImg = document.getElementById('video-mask');
 
-		cvs2.width  = $dom.width();
-		cvs2.height = $dom.height();
-		cvs2.style.width = "100%";
-		cvs2.style.height = "100%";
+		bufferCvs.width  = $dom.width();
+		bufferCvs.height = $dom.height();
+		bufferCvs.style.width = "100%";
+		bufferCvs.style.height = "100%";
 
 		video = sv.getVideo();
 		sv.drawVideo = draw;
@@ -26,17 +26,6 @@ function Main($dom,videoID){
 	}
 
 	function draw(){
-		ctx.clearRect(0,0,cvs.width,cvs.height);
-		ctx2.clearRect(0,0,cvs.width,cvs.height);
-		ctx2.globalCompositeOperation = 'source-in';
-		ctx2.drawImage(maskImg, 0, 0,cvs.width,cvs.height);
-		ctx2.globalCompositeOperation = 'xor';
-		ctx2.drawImage(video, 0, 0,cvs.width,cvs.height);
-
-		// ctx2.globalCompositeOperation = 'darken';
-		ctx.drawImage(cvs2, 0, 0,cvs.width,cvs.height);
-		
-		
 	}
 
 	this.play = function(){
