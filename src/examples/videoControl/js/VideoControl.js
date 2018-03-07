98543b6c082fe27aecd0d5bdbbab9c7b2d0f6f0a
wongeun heo <heowongeun@gmail.com>
2018-03-07 10:33:59 +0900

    updated module , added video controll at example

---
+++ src/examples/videoControl/js/VideoControl.js
@@ -0,0 +1,42 @@
+var $         = require('jQuery');
+var enableInlineVideo = require('iphone-inline-video');
+require('jquery-mousewheel')($);
+function VideoControl(video){
+	var frame = 0, oldFrame = -1;
+	var tweener;
+
+	function setup(){
+		$(video).on('mousewheel',function(e){
+			frame += -e.deltaY*0.01;
+			e.preventDefault();
+		});
+
+		var length = $('.btn').length;
+		$('.btn').each(function(i){
+			$(this).on('click',function(){
+				if(tweener)tweener.kill();
+				tweener = TweenLite.to(video,1,{currentTime:video.duration/length*i,ease:Power3.easeInOut,onUpdate:function(){
+					frame = video.currentTime;
+				}});
+			});
+		});
+		// video.addEventListener('canplay',onCanPlay);
+		render();
+	}
+
+	function render(){
+		requestAnimationFrame(render);
+		if(frame<0)frame = 0;
+		if(frame>video.duration)frame = video.duration;
+		if(frame != oldFrame)video.currentTime = frame;
+		oldFrame = frame;
+		videoBar.style.transform = "translateX("+(video.currentTime/video.duration)*100+"%)";
+	}
+
+
+	setup.call(this);
+	return this;
+}
+
+VideoControl.prototype.contructor = VideoControl;
+module.exports = VideoControl;
--- src/examples/videoControl/js/index.js
+++ src/examples/videoControl/js/index.js
@@ -1,5 +1,29 @@
+import {TweenMax, Power2, TimelineLite} from "gsap";
 (function () {
+	var $         = require('jQuery');
+  var throttle  = require('lodash/throttle');
+	var VideoControl = require('./VideoControl');
+	var enableInlineVideo = require('iphone-inline-video');
+
+	var pathName = window.location.pathname.split('/');
+	var pageName = pathName[pathName.length-1].split('.')[0];	
+
+
   $(document).ready(function(){
-    console.log('asd');
+    $('.'+pageName).addClass("active");
+    setup();
   });
+
+  function setup(){
+  	var video = document.getElementById('video');
+  	var videoBar = document.getElementById('videoBar')
+  	var tweener;
+
+  	if(pageName == 'videoControl'){
+	    var vc = new VideoControl(video);
+	    enableInlineVideo(video);
+	  }
+
+	  // render();
+  }
 }).call(this);
